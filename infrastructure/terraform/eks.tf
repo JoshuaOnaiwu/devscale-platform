22088413bd9bcf0b61025e742b618d8d9dac9432
JoshuaOnaiwu <onaiwujoshua@gmail.com>
2026-03-13 22:03:52 +0100

    Add GitOps nginx deployment

--- infrastructure/terraform/eks.tf
+++ infrastructure/terraform/eks.tf
@@ -10,6 +10,11 @@ module "eks" {
 
   enable_cluster_creator_admin_permissions = true
 
+  # Cluster endpoint settings
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+
   eks_managed_node_groups = {
     devscale_nodes = {
       instance_types = ["t3.small"]
